6f400cfdcd492fbf2d31c38d1633269ca713b780
Giovanny <giovanny.paz97c@gmail.com>
2023-01-27 16:10:33 -0500

    cambios 2

--- core/views.py
+++ core/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+from django.template.loader import get_template
 
 def login(request): # Vista login
-    
-    doc_externo=open('./core/plantillas/login.html')
-    plt=Template(doc_externo.read())
-    doc_externo.close()
-    ctx=Context()
-    documento=plt.render(ctx)
+    #urlcss = './core/plantillas/css/login.css'
+    #doc_externo=open('./core/plantillas/login.html')
+    #plt=Template(doc_externo.read())
+    #doc_externo.close()
+    #ctx=Context({"css":urlcss})
+    #documento=plt.render(ctx)
+
+    doc_externo=get_template('index.html')
+    documento=doc_externo.render({})
     return HttpResponse(documento)
 
 def home(request): # Vista Home
